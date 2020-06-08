66b827782a8f0548a68b2cfc67913d9b7a19dd2d
Maxi <maxignov@hotmail.com>
2020-06-07 21:32:43 -0300

    -

--- lugar/lugar.js
+++ lugar/lugar.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 
-const getLugarLatLang = async(dir) => {
-    const encodedUrl = encodeURI(dir);
+const getLugarLatLang = async(city) => {
+    const encodedCityName = encodeURI(city);
     const instance = axios.create({
-        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUrl}`,
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedCityName}`,
         headers: { "X-RapidAPI-Key": "cc5f314821mshc533cecb5832b8cp1201e2jsn6576f3d421f6" },
     });
 
     const respuesta = await instance.get();
-    if (respuesta.data.Results.length === 0) { throw new Error(`No hay resultados para ${dir}`) }
+    if (respuesta.data.Results.length === 0) { throw new Error(`No hay resultados para ${city}`) }
 
     const data = respuesta.data.Results[0];
     const direccion = data.name;
